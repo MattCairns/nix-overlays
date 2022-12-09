7839ed56aff10e8b05265924cb0e926c94d8499e
Matthew Cairns <mattrcairns@gmail.com>
2022-12-09 13:10:33 -0800

    fix: update to latest xfbs

--- pkgs/xplorer-flatbuffers/default.nix
+++ pkgs/xplorer-flatbuffers/default.nix
@@ -9,6 +9,7 @@ stdenv.mkDerivation rec {
   src = builtins.fetchGit {
     url = "git@gitlab.com:open-ocean-robotics/xplorer-vessel/libs/xplorer-flatbuffers.git";
     ref = "develop";   
+    rev = "60511c88aef6c37fb7a1292f71732b7651ef8afe";
   };
 
   nativeBuildInputs = [ cmake flatbuffers pkg-config ];
