f254d022e8b8bddc703d67c5d1e73361dbabe14a
Matthew Cairns <mattrcairns@gmail.com>
2023-04-25 12:40:34 -0700

    feat: add options for ktra service

--- modules/ktra.nix
+++ modules/ktra.nix
@@ -6,13 +6,42 @@ in {
     enable = mkEnableOption "ktra";
     arguments = mkOption {
       description = lib.mdDoc "Arguments to pass to ktra command";
+      default = "-c /etc/ktra/ktra.toml --db-dir-path /etc/ktra --dl-dir-path /etc/ktra --dl-path /etc/ktra";
+      example = "-c ~/.config/ktra/ktra.toml";
+      type = types.str;
+    };
+
+    remoteUrl = mkOption {
+      description = lib.mdDoc "The git repo containing your crate index.";
       default = "";
-      example = "-d -i shift";
+      example = "https://github.com/YOUR_USERNAME/YOUR_INDEX.git";
+      type = types.str;
+    };
+    username = mkOption {
+      description = lib.mdDoc "The username for the user that will be used to control the crate index";
+      default = "";
+      example = "john@doe.com";
+      type = types.str;
+    };
+    password = mkOption {
+      description = lib.mdDoc "The password for the user that will be used to control the crate index";
+      default = "";
+      example = "password123";
       type = types.str;
     };
   };
 
   config = mkIf cfg.enable {
+    environment.etc = {
+      "ktra/ktra.toml".text = ''
+        [index_config]
+        remote_url = "${cfg.remoteUrl}"
+        https_username = "${cfg.username}"
+        https_password = "${cfg.password}" 
+        branch = "main"
+      '';
+    };
+
     systemd.services.ktra = {
       description = "Ktra service";
       after = [ "network.target" ];
@@ -25,4 +54,3 @@ in {
     };
   };
 }
-
